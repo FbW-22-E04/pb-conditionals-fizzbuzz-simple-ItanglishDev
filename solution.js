72ef47bb9d6505a25b07303b5efef816bc41e1fa
chris <chrishethertonweb@gmail.com>
2022-07-12 13:03:27 +0200

    exercise done

---
+++ solution.js
@@ -0,0 +1,11 @@
+const number = 3;
+
+if (number % 3 == 0 && number % 5 === 0) {
+  console.log("FizzBuzz");
+} else if (number % 5 == 0) {
+  console.log("Buzz");
+} else if (number % 3 === 0) {
+  console.log("Fizz");
+} else {
+  console.log(number);
+}
